aeb07a732a72e049f37fcb60963995370918b9e7
rqp <798735878@qq.com>
2018-10-16 14:26:29 +0800

    第一版

---
+++ haichuang_ios_sdk.h
@@ -0,0 +1,13 @@
+//
+//  haichuang_ios_sdk.h
+//  haichuang_ios_sdk
+//
+//  Created by Hack on 2018/10/14.
+//  Copyright © 2018 Hack. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface haichuang_ios_sdk : NSObject
+-(void)allowConnect;
+@end
